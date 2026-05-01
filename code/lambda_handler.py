219aec06df01cd9276811e5b01043e36f5522b8a
sneha <sneha_gadre@yahoo.com>
2026-05-01 15:18:25 -0700

    cicd check code

--- code/lambda_handler.py
+++ code/lambda_handler.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "LAMBDA EXECUTED SUCCESSFULLY",
+            "message": "LAMBDA EXECUTED SUCCESSFULLY dfasf ",
             "event": event
         })
     }
